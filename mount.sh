01936c5e352dac422c0d7b1f03e774068899769c
hackosx64 <hackosx64@gmail.com>
2020-04-13 19:26:42 +0300

    Create mount.sh

---
+++ mount.sh
@@ -0,0 +1,32 @@
+# Сначала монтируем раздел
+echo
+mount /dev/nvme0n1p2 /mnt
+
+# Потом создаем на нем подтома
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@swap
+
+# Теперь мы демонтируем устройство
+umount /mnt
+
+mkdir -p /mnt/boot/efi /mnt/var/swap /mnt/home
+
+# Монтируемся
+
+# ZStandard ‒ это самый оптимальный метод сжатия. Если компьютер слабый, то лучше использовать lzo.
+mount -o rw,noatime,compress=zstd:3,ssd,space_cache,subvol=@ /dev/nvme0n1p2 /mnt
+mount /dev/nvme0n1p1 /mnt/boot/efi
+mount -o noatime,compress=zstd:3,space_cache,subvol=@home /dev/nvme0n1p2 /mnt/home
+mount -o rw,noatime,compress=no,ssd,space_cache,subvol=@swap /dev/nvme0n1p2 /mnt/var/swap
+
+# Создаем файл подкачки
+touch /mnt/var/swap/swapfile
+# chattr +C должна быть применена к пустому файлу!
+# Тут я перестраховываюсь, так как для подтома сжатие отключено
+chattr +C /mnt/var/swap/swapfile
+# С 16GB оперативной памяти нужно иметь файл подкачки минимум 4GB, 8 ‒ еще лучше (для гибернации нужно не менее 1/3 размера от RAM), а 32GB ‒ идеально
+fallocate -l 4G /mnt/var/swap/swapfile
+chmod 600 /mnt/var/swap/swapfile
+mkswap /mnt/var/swap/swapfile
+swapon /mnt/vra/swap/swapfile
