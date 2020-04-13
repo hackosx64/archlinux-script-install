57130be6c29da664fdebf813ee41001597718716
hackosx64 <hackosx64@gmail.com>
2020-04-13 22:40:45 +0300

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -23,3 +23,23 @@ mount -o rw,noatime,compress=zstd:3,ssd,space_cache,subvol=@ /dev/nvme0n1p2 /mnt
 mount /dev/nvme0n1p1 /mnt/boot/efi
 mount -o noatime,compress=zstd:3,space_cache,subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -o rw,noatime,compress=no,ssd,space_cache,subvol=@swap /dev/nvme0n1p2 /mnt/var/swap
+
+# Создаем файл подкачки
+touch /mnt/var/swap/swapfile
+# chattr +C должна быть применена к пустому файлу!
+# Тут я перестраховываюсь, так как для подтома сжатие отключено
+chattr +C /mnt/var/swap/swapfile
+# С 16GB оперативной памяти нужно иметь файл подкачки минимум 4GB, 8 ‒ еще лучше (для гибернации нужно не менее 1/3 размера от RAM), а 32GB ‒ идеально
+fallocate -l 4G /mnt/var/swap/swapfile
+chmod 600 /mnt/var/swap/swapfile
+mkswap /mnt/var/swap/swapfile
+swapon /mnt/vra/swap/swapfile
+
+
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+
+
+
+arch-chroot /mnt
